1b403d656bfb0d2ea1181135ba85a6af699a0533
charstiles <charfayestiles@gmail.com>
2019-11-13 14:40:56 -0500

    cleaning up code

--- ofApp.cpp
+++ ofApp.cpp
@@ -2,22 +2,12 @@
 using namespace cv;
 using namespace ofxCv;
 
-string folder = "italyPhotos";
-string path;// = "C:\\Users\\a\\Documents\\of_v0.10.0_vs2017_release\\apps\\myApps\\fastPhotos_threaded_2\\bin\\data\\" + folder;
-//string path = "C:\\Users\\a\\Documents\\of_v0.10.0_vs2017_release\\apps\\myApps\\fastPhotos_threaded\\bin\\data\\" + folder;
-
-int i;
-int j;
-ofColor colorOne(0);
-ofColor colorTwo(50);
-int pad = 100;
-bool skip;
-int pi;
-int pj;
-bool withPics;
-bool record;
 VideoWriter video;
+// uncomment for faster debugging, stops the prompt to ask user where video should be stored
 //VideoWriter video("C:\\Users\\a\\Documents\\of_v0.10.0_vs2017_release\\apps\\myApps\\fastPhotos_threaded_2\\bin\\data\\outcpp.mp4", CV_FOURCC('m', 'p', '4', 'v'), 10, Size(1920,1080));
+ofColor colorOne(0.);
+ofColor colorTwo(50.);
+
 void ofApp::setup() {
 	record = false;
 	ofSetLogLevel(OF_LOG_VERBOSE);
@@ -27,9 +17,7 @@ void ofApp::setup() {
 
 	//Check if the user opened a file
 	if (openFileResult.bSuccess) {
-
 		ofLogVerbose("User selected a file");
-
 	}
 	else {
 		ofLogVerbose("User hit cancel");
@@ -40,21 +28,24 @@ void ofApp::setup() {
 
 	path = openFileResult.getPath();
 
-	//cout << path;
 	frameRateAvg = 0;
 	withPics = true;
 	threadPerImage = true;
-	ofSetFrameRate(30);
+	ofSetFrameRate(25);
 	ofSetLogLevel(OF_LOG_VERBOSE);
 	ofSetVerticalSync(true);
 	skip = false;
-	i = 0;
-	j = (TOTAL / 2);
-	pi = 0;
-	pj = 0;
+
+	startBuffer = 0;
+	endBuffer = (TOTAL / 2);
+
+	pStartBuffer = 0;
+	pEndBuffer = 0;
+
 	sliderX = pad;
 	sliderY = ofGetHeight() / 2;
 	dir.open(path);
+
 	//populate the directory object
 	dir.allowExt("png");
 	dir.allowExt("jpg");
@@ -66,14 +57,13 @@ void ofApp::setup() {
 	int dirNum = dir.size();
 
 	if (total > dirNum) {
-
 		total = dirNum;
 	}
 
 	loading = false;
-	for (int i = 0; i < TOTAL; ++i) {
+
+	for (int i = 0; i < TOTAL; i++) {
 		images[i] = *(new ofImage());
-		//images[i].setUseTexture(false);
 		loaders[i].loadFromDisk(images[i], path + "\\" + dir.getName(i));
 		images[i].setUseTexture(false);
 	}
@@ -83,18 +73,20 @@ void ofApp::setup() {
 void ofApp::update() {
 	int h = ofGetHeight();
 
-	float height = 1.0*h;
+	float height = 1.0*h; // casting to float
 
 	speed = (1 - (float(sliderY - pad) / (height - (2 * pad))) - 0.5) * 2.;
-
 
 	if (speed < -1 || speed > 1) {
 		ofLog(OF_LOG_ERROR, "fastPhoto: speed out of range");
 	}
+	if (sliderY == ofGetHeight() / 2) {
+		speed = 0;
+	}
 
 	skip = false;
 
-	int m = ofLerp(2, 30, abs(abs(speed) - 0.5) * 2);
+	int m = ofLerp(2, 30, abs(abs(speed) - 0.5) * 2); // mapping from [-1,1] to [2,30]
 
 	if (abs(speed) < 0.5) { // most of the time advance
 		m = m / 2;
@@ -109,35 +101,34 @@ void ofApp::update() {
 		return;
 	}
 	if (speed >= 0) {
-		i = (i + 1) % TOTAL;
-		j = (j + 1) % dir.size();
+		startBuffer = (startBuffer + 1) % TOTAL;
+		endBuffer = (endBuffer + 1) % dir.size();
 	}
 	else {
-		i = (i - 1) % TOTAL;
-		j = (j - 1) % dir.size();
+		startBuffer = (startBuffer - 1) % TOTAL;
+		endBuffer = (endBuffer - 1) % dir.size();
 	}
 	if (withPics == false) {
 		return;
-	}
+	} 
 	if (ofGetFrameNum() >= (TOTAL / 2)) {
 		if (speed > 0) {
-			pi = (ofGetFrameNum() + (TOTAL / 2)) % TOTAL;
-			pj = j;
+			pStartBuffer = (ofGetFrameNum() + (TOTAL / 2)) % TOTAL;
+			pEndBuffer = endBuffer;
 		}
 		else {
-			pi = (ofGetFrameNum() - (TOTAL / 2)) % TOTAL;
-			pj = (j - TOTAL) % dir.size();
+			pStartBuffer = (ofGetFrameNum() - (TOTAL / 2)) % TOTAL;
+			pEndBuffer = (endBuffer - TOTAL) % dir.size();
 		}
-		if (pj < 0) { //for going backwards
-			pj += dir.size();
+		if (pEndBuffer < 0) { //for going backwards
+			pEndBuffer += dir.size();
 		}
-		if (pi < 0) { //for going backwards
-			pi += TOTAL;
+		if (pStartBuffer < 0) { //for going backwards
+			pStartBuffer += TOTAL;
 		}
-		//images[pi] = *( new ofImage());
-		//images[pi].setUseTexture(false);
-		loaders[pi].loadFromDisk(images[pi], path + "\\" + dir.getName(pj));
-		images[pi].setUseTexture(false);
+
+		loaders[pStartBuffer].loadFromDisk(images[pStartBuffer], path + "\\" + dir.getName(pEndBuffer));
+		images[pStartBuffer].setUseTexture(false);
 
 	}
 }
@@ -146,43 +137,44 @@ void ofApp::update() {
 void ofApp::draw() {
 	ofSetColor(255);
 	ofBackgroundGradient(colorOne, colorTwo, OF_GRADIENT_LINEAR);
-	if (i < 0) {
-		i += TOTAL;
-	}
-	if (loaders[i].isThreadRunning()) {
-		//ofLog(OF_LOG_NOTICE, "\nis it going to crash? \n");
+	
+	if (startBuffer < 0) {
+		startBuffer += TOTAL;
 	}
 
-	////draw Image
-	if (images[i].isAllocated()) {
-		loaders[i].lock();
-		images[i].setUseTexture(true);
+	//draw Image
+	if (images[startBuffer].isAllocated()) {
+		loaders[startBuffer].lock();
+		images[startBuffer].setUseTexture(true);
 
-		ofImage img = images[i];
+		ofImage img = images[startBuffer];
 		int w = (img.getWidth()) / (img.getHeight()) * ofGetHeight();
-
-		//img.update();
-		//img.resize(w, ofGetHeight());
 		img.draw((ofGetWidth() / 2) - w / 2, 0, w, ofGetHeight());
+
 		if (record) {
 			ofSetColor(255, 0, 0);
 			ofEllipse(ofGetWidth() - 120, 120, 50, 50);
-			frames.push_back(j);
+			frames.push_back(endBuffer);
 			frameRateAvg += ofGetFrameRate();
 		}
-		loaders[i].unlock();
-
+		loaders[startBuffer].unlock();
 	}
 	else {
-		cout << "viewer too fast!2";
+		cout << "viewer too fast!";
 	}
-	ofSetColor(255);
+
 	// draw the FPS
+	ofSetColor(100);
 	ofDrawRectangle(0, ofGetHeight() - 50, 100, 50);
 
+	ofSetColor(200);
+	ofRect(pad / 2, (ofGetHeight() / 2) - (25), pad, 50);
+
+	ofSetColor(100);
+	ofEllipse(sliderX, sliderY, 40, 40);
+	ofSetColor(255);
 	ofEllipse(sliderX, sliderY, 30, 30);
 
-	ofSetColor(0);
 	ofDrawBitmapString("Speed:" + ofToString(speed) + "\nFrameRate" + ofToString(ofGetFrameRate(), 0), 5, ofGetHeight() - 30);
 
 }
@@ -190,9 +182,12 @@ void ofApp::draw() {
 void ofApp::writeMovie(string fileName) {
 	int fr = frameRateAvg / frames.size();
 
-	//video.open(fileName + ".avi", CV_FOURCC('M', 'J', 'P', 'G'), fr, Size(ofGetWidth(), ofGetHeight()));
+	// save movie as a avi, the mjpg codec comes with most windows machines
 	video.open(fileName + ".avi", CV_FOURCC('M', 'J', 'P', 'G'), fr, Size(ofGetWidth(), ofGetHeight()));
+	
+	//this is another codec:
 	//video.open(fileName + ".avi", CV_FOURCC('M', 'R', 'L', 'E'), fr, Size(ofGetWidth(), ofGetHeight()), true);
+	
 	for (int i = 0; i < frames.size(); i++) {
 		ofImage img;
 		img.load(path + "\\" + dir.getName(frames[i]));
@@ -202,17 +197,14 @@ void ofApp::writeMovie(string fileName) {
 		int border = floor((ofGetWidth() - w) / 2.0);
 		Mat imgMat = toCv(img);
 		
-		Mat resized(ofGetHeight(), ofGetWidth(), imgMat.type());//imgMat.depth());
+		Mat resized(ofGetHeight(), ofGetWidth(), imgMat.type());
 
 		if (ofGetHeight() - w % 2 == 1) {
-			//cout << w + border + border + 1;
 			copyMakeBorder(imgMat, resized, 0, 0, border + 1, border, BORDER_CONSTANT);
 		}
 		else {
-			//cout << w + border + border;
 			copyMakeBorder(imgMat, resized, 0, 0, border, border, BORDER_CONSTANT);
 		}
-		//resized *= 1. / 255;
 		Mat fin;
 		cvtColor(resized,fin,CV_BGR2RGB);
 		video.write(fin);
@@ -224,42 +216,31 @@ void ofApp::writeMovie(string fileName) {
 
 //--------------------------------------------------------------
 void ofApp::exit() {
-	/*for (int i = 0; i < total; ++i) {
-		loaders[i].waitForThread();
-		loaders[i].stopThread();
-	}*/
+	for (int i = 0; i < TOTAL; ++i) { // free image memory
+		images[i].clear();
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
-	if (key == 'm') {
-		record = true;
+	if (key == ' ') { // hit space to record you flipping through photos
+		record = !record; // hit space again to stop
+
+			if (record == false) {
+
+				ofFileDialogResult result = ofSystemSaveDialog("fastPhotoMovie_" + ofGetTimestampString("%Y-%m-%d-%H-%M"), "Save Fast Photo Movie");
+				if (result.bSuccess) {
+
+					writeMovie(result.getPath());
+					// save your file to `path`
+				}
+				else {
+					frames.clear();
+					frameRateAvg = 0;
+				}
+			}
+		
 	}
-	if (key == 'n') {
-		record = false;
-
-		ofFileDialogResult result = ofSystemSaveDialog("fastPhotoMovie_" + ofGetTimestampString("%Y-%m-%d-%H-%M"), "Save Fast Photo Movie");
-		if (result.bSuccess) {
-
-			writeMovie(result.getPath());
-
-			// save your file to `path`
-		}
-		else {
-			frames.clear();
-			frameRateAvg = 0;
-		}
-	}
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y) {
 
 }
 
@@ -270,23 +251,12 @@ void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) {
-
-}
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y) {
+	if (abs(y - ofGetHeight() / 2) < pad/2) {
+		sliderY = ofGetHeight() / 2;
+	}
 
 }
 
--- ofApp.h
+++ ofApp.h
@@ -17,19 +17,14 @@ class ofApp : public ofBaseApp{
 		void writeMovie(string fileName);
 		
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);	
 
 		string appPath = ofFilePath::getAbsolutePath(ofFilePath::getCurrentExeDir());
-		//string appPathBase = 
+		
 		bool loading;
 		bool threadPerImage;
 		
@@ -41,5 +36,20 @@ class ofApp : public ofBaseApp{
 		vector<int> frames;
 		float sliderX, sliderY;
 		float speed = 0.0; // range from -1 to 1
+		int startBuffer;
+		int endBuffer;
+
+		int pad = 100;
+		bool skip;
+		int pStartBuffer; // previous start buffer
+		int pEndBuffer; // previous end buffer
+
+		string path;
+		// uncomment these two lines for debugging, bypasses the part where you have to choose the folder manually
+		// string folder = "italyPhotos";
+		// string path = "C:\\Users\\a\\Documents\\of_v0.10.0_vs2017_release\\apps\\myApps\\fastPhotos_threaded_2\\bin\\data\\" + folder;
+
+		bool withPics;
+		bool record;
 
 };
